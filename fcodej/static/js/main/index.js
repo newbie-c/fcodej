dbf6c3c338af340744fa0f698ada0a68ad313664
AndreyVM <webmaster@codej.ru>
2023-02-28 08:41:36 +0500

    Show the registration form

--- fcodej/static/js/main/index.js
+++ fcodej/static/js/main/index.js
@@ -1,7 +1,6 @@
 $(function() {
   let token = window.localStorage.getItem('token');
   let data = token ? {token: token} : {};
-  console.log(window.location.hash);
   $.ajax({
     method: 'GET',
     url: '/api/index',
@@ -19,18 +18,54 @@ $(function() {
     },
     dataType: 'json'
   });
+  $('body').on('click', '#rcaptcha-reload', function() {
+    $(this).blur();
+    $.ajax({
+      method: 'GET',
+      url: '/api/captcha',
+      success: function(data) {
+        $('#rcaptcha-field')
+          .attr({"style": 'background:url(' + data.url +')'});
+        $('#rsuffix').val(data.captcha);
+        $('#rcaptcha').focus();
+      },
+      dataType: 'json'
+    });
+  });
   $('body').on('click', '#lcaptcha-reload', function() {
     $(this).blur();
     $.ajax({
       method: 'GET',
       url: '/api/captcha',
       success: function(data) {
-        $('#lcaptcha-field').attr({"style": 'background:url(' + data.url +')'});
+        $('#lcaptcha-field')
+          .attr({"style": 'background:url(' + data.url +')'});
         $('#lsuffix').val(data.captcha);
         $('#lcaptcha').focus();
       },
       dataType: 'json'
     });
+  });
+  $('body').on('click', '#reg', function() {
+    if (!$('#regf').length) {
+      $.ajax({
+        method: 'GET',
+        url: '/api/captcha',
+        success: function(data) {
+          let form = Mustache.render($('#regt').html(), data);
+          $('#main-container').append(form);
+          $('#main-container .alert').slideUp('fast');
+          $('#loginf').slideUp('slow');
+        },
+        dataType: 'json'
+      });
+    } else {
+      $('#rcaptcha-reload').trigger('click');
+      $('#regf').slideDown('slow');
+      if ($('#loginf').length) $('#loginf').slideUp('slow');
+    }
+    let col = $(this).parents('.navbar-collapse');
+    if (col.hasClass('in')) col.removeClass('in');
   });
   $('body').on('click', '#login', function() {
     if (!$('#loginf').length) {
@@ -41,9 +76,14 @@ $(function() {
           let form = Mustache.render($('#logint').html(), data);
           $('#main-container').append(form);
           $('#main-container .alert').slideUp('fast');
+          $('#regf').slideUp('slow');
         },
         dataType: 'json'
       });
+    } else {
+      $('#lcaptcha-reload').trigger('click');
+      $('#loginf').slideDown('slow');
+      if ($('#regf').length) $('#regf').slideUp('slow');
     }
     let col = $(this).parents('.navbar-collapse');
     if (col.hasClass('in')) col.removeClass('in');
