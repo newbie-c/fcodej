2031d11c6c8a6c32089e6f877777e4dd3f0ae53f
AndreyVM <webmaster@codej.ru>
2023-03-05 09:02:00 +0500

    Make it simpler
    I'm just looking for a solution. Doing this is
    a bad expirience when you don't know yet what you want.

--- fcodej/static/js/login.js
+++ fcodej/static/js/login.js
@@ -1,33 +1,18 @@
 function login() {
   if (!$('#loginf').length) {
-    $.ajax({
+    $.ajax ({
       method: 'GET',
       url: '/api/captcha',
       success: function(data) {
         let form = Mustache.render($('#logint').html(), data);
         $('#main-container').append(form);
-        $('#main-container .top-flashed-block').slideUp('fast');
-        $('.idef').slideUp('fast', function() {
-          if ($('#regf').length) {
-            $('#regf').slideUp('fast', function() {
-              $('#loginf').slideDown('slow');
-            });
-          } else {
-            $('#loginf').slideDown('slow');
-          }
+        $('#loginf').siblings().each(function() {
+          $(this).slideUp('slow', function() { $(this).remove(); });
         });
+        $('#loginf').slideDown('slow');
       },
       dataType: 'json'
     });
-  } else {
-    $('#lcaptcha-reload').trigger('click');
-    if ($('#regf').length) {
-      $('#regf').slideUp('fast', function() {
-        $('#loginf').slideDown('slow');
-      });
-    } else {
-      $('#loginf').slideDown('slow');
-    }
   }
   let col = $(this).parents('.navbar-collapse');
   if (col.hasClass('in')) col.removeClass('in');
--- fcodej/static/js/reg.js
+++ fcodej/static/js/reg.js
@@ -6,29 +6,13 @@ function reg() {
       success: function(data) {
         let form = Mustache.render($('#regt').html(), data);
         $('#main-container').append(form);
-        if ($('#loginf').length) $('#loginf').slideUp('slow');
-        $('#main-container .top-flashed-block').slideUp('fast');
-        $('.idef').slideUp('fast', function() {
-          if ($('#loginf').length) {
-            $('#loginf').slideUp('fast', function() {
-              $('#regf').slideDown('slow');
-            });
-          } else {
-            $('#regf').slideDown('slow');
-          }
+        $('#regf').siblings().each(function () {
+          $(this).slideUp('slow', function() { $(this).remove(); });
         });
+        $('#regf').slideDown('slow');
       },
       dataType: 'json'
     });
-  } else {
-    $('#rcaptcha-reload').trigger('click');
-    if ($('#loginf').length) {
-      $('#loginf').slideUp('fast', function() {
-        $('#regf').slideDown('slow');
-      });
-    } else {
-      $('#regf').slideDown('slow');
-    }
   }
   let col = $(this).parents('.navbar-collapse');
   if (col.hasClass('in')) col.removeClass('in');
